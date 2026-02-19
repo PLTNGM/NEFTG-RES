0ab160695785f0d0f6ad270954b894e192d6fdb2
Platon Gimp <platongimp96@gmail.com>
2026-02-19 11:44:15 +0500

    fix header

--- back/app.py
+++ back/app.py
@@ -1,11 +1,20 @@
 import os
+import time
+from threading import Timer
+from functools import lru_cache
 from flask import Flask, render_template
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import requests
 
-app = Flask(__name__, template_folder='../',
-            static_folder='../styles',
+# Узнаем путь к папке, где лежит сам app.py (это папка back)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+# Указываем, что шаблоны и статика на один уровень выше
+root_dir = os.path.join(base_dir, '..')
+
+app = Flask(__name__, 
+            template_folder=root_dir, 
+            static_folder=os.path.join(root_dir, 'styles'),
             static_url_path='/styles')
 
 DB_CONFIG = {
@@ -21,6 +30,7 @@ def connection():
 
 BOT_TOKEN = '8574045363:AAEbZR8euBvfwthO8HfEt45Aq2cilhLk3xw'
 
+@lru_cache(maxsize=128)
 def get_tg_file_url(file_id):
     if not file_id:
         return None
@@ -61,11 +71,44 @@ def get_posts():
     conn.close()
     return posts
 
-@app.route('/')
+@app.route("/")
 def index():
-    all_posts = get_posts()
-    # Мы передаем переменную all_posts в HTML под именем 'posts'
-    return render_template('activity.html', posts=all_posts, get_url=get_tg_file_url)
+    return render_template('index.html')
+
+@app.route('/activity')
+def activity():
+    raw_posts = get_posts() 
+    # "Прогреваем" ссылки для медиа
+    for post in raw_posts:
+        if post['media']:
+            for item in post['media']:
+                # Получаем ссылку через твою функцию с кэшем
+                item['url'] = get_tg_file_url(item['file_id'])
+                
+    return render_template('activity.html', posts=raw_posts)
+
+@app.route("/Merch")
+def Merch():
+    return render_template('Merch.html')
+
+@app.route("/paschalko")
+def paschalko():
+    return render_template("paschalko.html")
+
+def clear_cache():
+    get_tg_file_url.cache_clear()
+    print("Кэш ссылок Telegram очищен!")
+    # Запускаем таймер снова на 50 минут (3000 секунд)
+    Timer(3000, clear_cache).start()
+
+cache_started = False
+
+@app.before_request
+def start_timer():
+    global cache_started
+    if not cache_started:
+        clear_cache()
+        cache_started = True
 
 if __name__ == '__main__':
     app.run(debug=True)
--- back/main.py
+++ back/main.py
@@ -2,7 +2,7 @@ import telebot
 import psycopg2
 from psycopg2 import sql
 
-ADMIN_ID = 1306570088
+ADMIN_ID = [1306570088, 1341021324]
 
 DB_CONFIG = {
     "database": "NEFTG",
@@ -56,103 +56,103 @@ def init_db():
 
 init_db()
 
-@bot.channel_post_handler(content_types=['photo', 'video', 'text'])
-def handle_post(message):
-    post_id = message.message_id
-    channel_id = message.chat.id
-    m_group_id = message.media_group_id  # Тот самый ключ к альбомам
-    text = message.caption or message.text or ""
+# @bot.channel_post_handler(content_types=['photo', 'video', 'text'])
+# def handle_post(message):
+#     post_id = message.message_id
+#     channel_id = message.chat.id
+#     m_group_id = message.media_group_id  # Тот самый ключ к альбомам
+#     text = message.caption or message.text or ""
 
-    conn = connection()
-    cur = conn.cursor()
+#     conn = connection()
+#     cur = conn.cursor()
 
-    try:
-        target_post_id = post_id
+#     try:
+#         target_post_id = post_id
 
-        # ЛОГИКА СКЛЕЙКИ АЛЬБОМА
-        if m_group_id:
-            # Проверяем, не залетало ли уже что-то из этого альбома
-            cur.execute('SELECT post_id FROM post_media WHERE media_group_id = %s LIMIT 1', (m_group_id,))
-            result = cur.fetchone()
-            if result:
-                target_post_id = result[0]
+#         # ЛОГИКА СКЛЕЙКИ АЛЬБОМА
+#         if m_group_id:
+#             # Проверяем, не залетало ли уже что-то из этого альбома
+#             cur.execute('SELECT post_id FROM post_media WHERE media_group_id = %s LIMIT 1', (m_group_id,))
+#             result = cur.fetchone()
+#             if result:
+#                 target_post_id = result[0]
 
-        # 1. Работаем с основным постом
-        # Используем UPDATE, чтобы если текст пришел не в первом сообщении, он все равно сохранился
-        cur.execute('''
-            INSERT INTO posts (post_id, chanel_id, post_text)
-            VALUES (%s, %s, %s)
-            ON CONFLICT (post_id) DO UPDATE SET 
-            post_text = CASE WHEN EXCLUDED.post_text <> '' THEN EXCLUDED.post_text ELSE posts.post_text END
-        ''', (target_post_id, channel_id, text))
+#         # 1. Работаем с основным постом
+#         # Используем UPDATE, чтобы если текст пришел не в первом сообщении, он все равно сохранился
+#         cur.execute('''
+#             INSERT INTO posts (post_id, chanel_id, post_text)
+#             VALUES (%s, %s, %s)
+#             ON CONFLICT (post_id) DO UPDATE SET 
+#             post_text = CASE WHEN EXCLUDED.post_text <> '' THEN EXCLUDED.post_text ELSE posts.post_text END
+#         ''', (target_post_id, channel_id, text))
 
-        # 2. Определяем медиа
-        file_id = None
-        m_type = None
-        if message.content_type == "photo":
-            file_id = message.photo[-1].file_id
-            m_type = "photo"
-        elif message.content_type == "video":
-            file_id = message.video.file_id
-            m_type = "video"
+#         # 2. Определяем медиа
+#         file_id = None
+#         m_type = None
+#         if message.content_type == "photo":
+#             file_id = message.photo[-1].file_id
+#             m_type = "photo"
+#         elif message.content_type == "video":
+#             file_id = message.video.file_id
+#             m_type = "video"
 
-        # 3. Сохраняем медиа (если оно есть)
-        if file_id:
-            cur.execute('''
-                INSERT INTO post_media (post_id, file_id, media_type, media_group_id)
-                VALUES (%s, %s, %s, %s)
-                ON CONFLICT DO NOTHING
-            ''', (target_post_id, file_id, m_type, m_group_id))
+#         # 3. Сохраняем медиа (если оно есть)
+#         if file_id:
+#             cur.execute('''
+#                 INSERT INTO post_media (post_id, file_id, media_type, media_group_id)
+#                 VALUES (%s, %s, %s, %s)
+#                 ON CONFLICT DO NOTHING
+#             ''', (target_post_id, file_id, m_type, m_group_id))
 
-        conn.commit()
-        print(f"✅ Пост {target_post_id} обработан (Media: {m_type})")
+#         conn.commit()
+#         print(f"✅ Пост {target_post_id} обработан (Media: {m_type})")
 
-    except Exception as e:
-        print(f"❌ Ошибка в handle_post: {e}")
-        conn.rollback()
-    finally:
-        cur.close()
-        conn.close()
+#     except Exception as e:
+#         print(f"❌ Ошибка в handle_post: {e}")
+#         conn.rollback()
+#     finally:
+#         cur.close()
+#         conn.close()
 
-@bot.message_reaction_count_handler()
-def reaction_change(update):
-    conn = connection()
-    cur = conn.cursor()
+# @bot.message_reaction_count_handler()
+# def reaction_change(update):
+#     conn = connection()
+#     cur = conn.cursor()
 
-    post_id = update.message_id
-    reaction_list = update.reactions
+#     post_id = update.message_id
+#     reaction_list = update.reactions
 
-    try:
-        # ПРОВЕРКА: есть ли вообще такой пост в нашей таблице posts?
-        cur.execute('SELECT 1 FROM posts WHERE post_id = %s', (post_id,))
-        post_exists = cur.fetchone()
+#     try:
+#         # ПРОВЕРКА: есть ли вообще такой пост в нашей таблице posts?
+#         cur.execute('SELECT 1 FROM posts WHERE post_id = %s', (post_id,))
+#         post_exists = cur.fetchone()
 
-        if not post_exists:
-            # Если поста нет, мы не можем добавить реакцию из-за Foreign Key.
-            # Просто выходим, чтобы не было ошибки в консоли.
-            return
+#         if not post_exists:
+#             # Если поста нет, мы не можем добавить реакцию из-за Foreign Key.
+#             # Просто выходим, чтобы не было ошибки в консоли.
+#             return
 
-        # Если пост найден, обновляем реакции как обычно
-        cur.execute('DELETE FROM reactions WHERE post_id = %s', (post_id,))
+#         # Если пост найден, обновляем реакции как обычно
+#         cur.execute('DELETE FROM reactions WHERE post_id = %s', (post_id,))
 
-        for react in reaction_list:
-            emoji = react.type.emoji if react.type.type == "emoji" else 'custom'
-            count = react.total_count
+#         for react in reaction_list:
+#             emoji = react.type.emoji if react.type.type == "emoji" else 'custom'
+#             count = react.total_count
 
-            cur.execute('''
-                INSERT INTO reactions (post_id, emoji, counter)
-                VALUES (%s, %s, %s)
-            ''', (post_id, emoji, count))
+#             cur.execute('''
+#                 INSERT INTO reactions (post_id, emoji, counter)
+#                 VALUES (%s, %s, %s)
+#             ''', (post_id, emoji, count))
             
-        conn.commit()
+#         conn.commit()
     
-    except Exception as e:
-        # Теперь сюда будут падать только реальные ошибки (например, обрыв связи с БД)
-        print(f"Ошибка при обновлении реакций: {e}")
-        conn.rollback()
-    finally:
-        cur.close()
-        conn.close()
+#     except Exception as e:
+#         # Теперь сюда будут падать только реальные ошибки (например, обрыв связи с БД)
+#         print(f"Ошибка при обновлении реакций: {e}")
+#         conn.rollback()
+#     finally:
+#         cur.close()
+#         conn.close()
 
 
 # Словарь для хранения связки: {media_group_id: original_post_id}
@@ -163,70 +163,84 @@ album_storage = {}
     content_types=['text', 'photo', 'video']
 )
 def handle_imported_post(message):
-    if message.from_user.id != ADMIN_ID:
-        return
-
-    old_post_id = message.forward_from_message_id
-    m_group_id = message.media_group_id
-    post_text = message.text or message.caption or ""
-    
-    # Решаем, под каким ID сохранять медиа
-    # Если это альбом, и мы уже сохранили его первое сообщение
-    if m_group_id and m_group_id in album_storage:
-        target_post_id = album_storage[m_group_id]
-    else:
-        # Если это не альбом или первое сообщение альбома
-        target_post_id = old_post_id
-        if m_group_id:
-            album_storage[m_group_id] = old_post_id
-
-    conn = connection()
-    cur = conn.cursor()
-    try:
-        # 1. Создаем или обновляем основной пост (текст берем только если он есть)
-        if post_text or not m_group_id or (m_group_id not in album_storage):
-             cur.execute('''
-                INSERT INTO posts (post_id, chanel_id, post_text)
-                VALUES (%s, %s, %s)
-                ON CONFLICT (post_id) DO UPDATE SET 
-                post_text = CASE WHEN EXCLUDED.post_text <> '' THEN EXCLUDED.post_text ELSE posts.post_text END
-            ''', (target_post_id, message.forward_from_chat.id, post_text))
+    if message.from_user.id in ADMIN_ID:
+        old_post_id = message.forward_from_message_id
+        m_group_id = message.media_group_id
+        post_text = message.text or message.caption or ""
         
-        # 2. Сохраняем медиа, привязывая его к target_post_id
-        file_id = None
-        media_type = None
+        # Решаем, под каким ID сохранять медиа
+        # Если это альбом, и мы уже сохранили его первое сообщение
+        if m_group_id and m_group_id in album_storage:
+            target_post_id = album_storage[m_group_id]
+        else:
+            # Если это не альбом или первое сообщение альбома
+            target_post_id = old_post_id
+            if m_group_id:
+                album_storage[m_group_id] = old_post_id
 
-        if message.photo:
-            file_id = message.photo[-1].file_id
-            media_type = 'photo'
-        elif message.video:
-            file_id = message.video.file_id
-            media_type = 'video'
-        
-        if file_id:
-            cur.execute('''
-                INSERT INTO post_media (post_id, file_id, media_type)
-                VALUES (%s, %s, %s)
-                ON CONFLICT DO NOTHING
-            ''', (target_post_id, file_id, media_type))
+        conn = connection()
+        cur = conn.cursor()
+        try:
+            # 1. Создаем или обновляем основной пост (текст берем только если он есть)
+            if post_text or not m_group_id or (m_group_id not in album_storage):
+                cur.execute('''
+                    INSERT INTO posts (post_id, chanel_id, post_text)
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT (post_id) DO UPDATE SET 
+                    post_text = CASE WHEN EXCLUDED.post_text <> '' THEN EXCLUDED.post_text ELSE posts.post_text END
+                ''', (target_post_id, message.forward_from_chat.id, post_text))
+            
+            # 2. Сохраняем медиа, привязывая его к target_post_id
+            file_id = None
+            media_type = None
 
-        conn.commit()
-        print(f"✅ Обработано: {media_type if file_id else 'text'} для поста {target_post_id}")
-        
-    except Exception as e:
-        print(f"❌ Ошибка: {e}")
-        conn.rollback()
-    finally:
-        cur.close()
-        conn.close()
+            if message.photo:
+                file_id = message.photo[-1].file_id
+                media_type = 'photo'
+            elif message.video:
+                file_id = message.video.file_id
+                media_type = 'video'
+            
+            if file_id:
+                cur.execute('''
+                    INSERT INTO post_media (post_id, file_id, media_type)
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT DO NOTHING
+                ''', (target_post_id, file_id, media_type))
 
-if __name__ == "__main__":
-    try:
-        init_db()
+            conn.commit()
+            print(f"✅ Обработано: {media_type if file_id else 'text'} для поста {target_post_id}")
+            
+        except Exception as e:
+            print(f"❌ Ошибка: {e}")
+            conn.rollback()
+        finally:
+            cur.close()
+            conn.close()
 
-        print("🚀 Бот вышел на связь...")
-        bot.infinity_polling(allowed_updates=['message', 'channel_post', 'message_reaction_count'])
+@bot.message_handler(commands=['clear'])
+def clear_database(message):
+    if message.from_user.id in ADMIN_ID:
+        conn = connection()
+        cur = conn.cursor()
+        try:
+            cur.execute('TRUNCATE posts, post_media, reactions RESTART IDENTITY CASCADE;')
+            conn.commit()
+            bot.reply_to(message, "🗑 База полностью очищена!")
+        except Exception as e:
+            bot.reply_to(message, f"ошибка очистки: {e}")
+        finally:
+            cur.close()
+            conn.close()
 
 
-    except Exception as e:
-        print(f"❌ Критическая ошибка при запуске: {e}")
+    if __name__ == "__main__":
+        try:
+            init_db()
+
+            print("🚀 Бот вышел на связь...")
+            bot.infinity_polling(allowed_updates=['message', 'channel_post', 'message_reaction_count'])
+
+
+        except Exception as e:
+            print(f"❌ Критическая ошибка при запуске: {e}")
